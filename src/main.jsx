e650bdcc64336c7205da13497710e71262f6e837
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-24 16:21:38 -0300

    Started cine tag project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './globals.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Olá mundo!</h1>
+  </React.StrictMode>,
+)
